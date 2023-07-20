efc434c0f03e779331aa80fac357ac266ce6caa0
Wyatt <136139037+CodeGLW@users.noreply.github.com>
2023-07-20 12:40:01 -0700

    Add files via upload
    Repeating characters in while loops

---
+++ whileWave.py
@@ -0,0 +1,11 @@
+i = 1
+for i in range (4):
+    while i <= 4:
+        print (i * "*")
+        i += 1
+    while i >= 1:
+        print (i * "*")
+        i -= 1
+
+# COWABUNGA DUUUUDE
+# surf's up, bro
