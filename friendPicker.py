c1ecfdd6bc7b2c36306035666c9b40b8aa5ada44
Wyatt <136139037+CodeGLW@users.noreply.github.com>
2023-07-20 23:58:07 -0700

    Add files via upload
    This is just an idea I have that I want to expand on later (probably outside this video tutorial)

---
+++ friendPicker.py
@@ -0,0 +1,5 @@
+# make a program that calculates which friend I should start a conversation with
+# closer friends are more likely to be picked than others
+# have a way to confirm that I want the friend it chose or have the option to rerun
+# if I do talk to the friend, their value is decreased
+# this data will be stored in a file that I can write over later to keep continuity
