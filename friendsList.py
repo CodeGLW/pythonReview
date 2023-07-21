e3aef70dbf632bf80261dd907ee8ceb6b481e275
Wyatt <136139037+CodeGLW@users.noreply.github.com>
2023-07-21 00:03:57 -0700

    Update friendsList.py
    added a for loop

--- friendsList.py
+++ friendsList.py
@@ -1,8 +1,11 @@
-friends = ["Daniel", "Oviya", "Nason", "Caz", "Stephan", "Olivia", "Bianca", "Jen", "Eddie", "discord gc", "Teddy", "Shinjoe", "Yiyi", "Marina", "Anika", "Theo", "Bex", "Sam", "Linus", "Neil", "Logan", "Gary", "Nick", "Victor", "Ellise", "Sasha", "Hausen", "Aster"]
+friends = ["Daniel", "Oviya", "Nason", "Caz", "Stephan", "Olivia", "Bianca", "Jen", "Eddie", "Lin", "Teddy", "Shinjoe", "Yiyi", "Marina", "Anika", "Theo", "Bex", "Sam", "Linus", "Neil", "Logan", "Gary", "Nick", "Victor", "Ellise", "Sasha", "Hausen", "Aster"]
+print("Here are a bunch of my friends!")
+for name in friends:
+    print(name)
 print("I love " + friends[0] + " and " + friends[1])
 print("The last friend on my list is " + friends[-1])
 friends[0] = "Cedric"
-print("Nevermind! I didn't mean 'daniel', I meant " + friends[0])
+print("Nevermind! I didn't mean 'Daniel', I meant " + friends[0])
 print("The first 5 people I last messaged on Instagram are as follows: " + str(friends[0:5]))
 friends.append("Riley")
 friends.insert(5, "Charlie")
